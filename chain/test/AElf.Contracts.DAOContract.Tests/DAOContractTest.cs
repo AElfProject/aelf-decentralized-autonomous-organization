0f690c2671644995e16a0a77733a123898851123
yiqi <yiqi.zhao@aelf.io>
2020-04-13 10:12:10 +0800

    DAOManagementTest

--- chain/test/AElf.Contracts.DAOContract.Tests/DAOContractTest.cs
+++ chain/test/AElf.Contracts.DAOContract.Tests/DAOContractTest.cs
@@ -23,30 +23,55 @@ namespace AElf.Contracts.DAOContract
         }
 
         [Fact]
-        public async Task DAOManagement()
+        public async Task DAOManagementTest()
         {
             await InitialDAOContract();
 
-            // Alice want to join DAO.
             var aliceParliamentStub = GetParliamentContractStub(AliceKeyPair);
-            var proposalId = (await aliceParliamentStub.CreateProposal.SendAsync(new CreateProposalInput
-            {
-                OrganizationAddress = ParliamentDefaultOrganizationAddress,
-                ContractMethodName = nameof(DAOContractStub.ProposeJoin),
-                ExpiredTime = TimestampHelper.GetUtcNow().AddHours(1),
-                Params = new StringValue
-                {
-                    Value = AliceKeyPair.PublicKey.ToHex()
-                }.ToByteString(),
-                ToAddress = DAOContractAddress
-            })).Output;
 
-            await ParliamentApproveAsync(proposalId);
-            await aliceParliamentStub.Release.SendAsync(proposalId);
-            
-            // Check DAO member list.
-            var memberList = (await DAOContractStub.GetDAOMemberList.CallAsync(new Empty())).Value;
-            memberList.ShouldContain(AliceAddress);
+            // Alice want to join DAO.
+            {
+                var proposalId = (await aliceParliamentStub.CreateProposal.SendAsync(new CreateProposalInput
+                {
+                    OrganizationAddress = ParliamentDefaultOrganizationAddress,
+                    ContractMethodName = nameof(DAOContractStub.ProposeJoin),
+                    ExpiredTime = TimestampHelper.GetUtcNow().AddHours(1),
+                    Params = new StringValue
+                    {
+                        Value = AliceKeyPair.PublicKey.ToHex()
+                    }.ToByteString(),
+                    ToAddress = DAOContractAddress
+                })).Output;
+
+                await ParliamentApproveAsync(proposalId);
+                await aliceParliamentStub.Release.SendAsync(proposalId);
+
+                // Check DAO member list.
+                var memberList = (await DAOContractStub.GetDAOMemberList.CallAsync(new Empty())).Value;
+                memberList.ShouldContain(AliceAddress);
+            }
+
+            // Alice want to quit DAO.
+            {
+                var proposalId = (await aliceParliamentStub.CreateProposal.SendAsync(new CreateProposalInput
+                {
+                    OrganizationAddress = ParliamentDefaultOrganizationAddress,
+                    ContractMethodName = nameof(DAOContractStub.Quit),
+                    ExpiredTime = TimestampHelper.GetUtcNow().AddHours(1),
+                    Params = new StringValue
+                    {
+                        Value = AliceKeyPair.PublicKey.ToHex()
+                    }.ToByteString(),
+                    ToAddress = DAOContractAddress
+                })).Output;
+
+                await ParliamentApproveAsync(proposalId);
+                await aliceParliamentStub.Release.SendAsync(proposalId);
+                
+                // Check DAO member list.
+                var memberList = (await DAOContractStub.GetDAOMemberList.CallAsync(new Empty())).Value;
+                memberList.ShouldNotContain(AliceAddress);
+            }
         }
     }
 }
